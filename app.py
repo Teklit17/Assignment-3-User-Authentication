dc9bdc81d0a1b846521f114ab08da6314b569275
teklit amanuel <teklitag@uia.no>
2023-10-31 11:49:19 +0100

    user authentication added

--- app.py
+++ app.py
@@ -1,26 +1,108 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import bcrypt
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SECRET_KEY'] = 'your-secret-key'  # Replace with a secure secret key
+
+# Initialize SQLAlchemy and Flask-Migrate
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)  # Added Flask-Migrate
+migrate = Migrate(app, db)
+
+# Configure the login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 
+# Define RegistrationForm
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+# Define LoginForm
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+# Define a User class
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+# Create a user loader function
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Create a function to hash passwords
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
+
+# Define Post class for the database
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     content = db.Column(db.Text)
 
+# Create a route for user registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
+        # Check if the username is already in use
+        if User.query.filter_by(username=username).first():
+            flash('Username is already taken.', 'error')
+        else:
+            hashed_password = hash_password(password)
+            user = User(username=username, password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Registration successful. You can now log in.', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# Create a route for user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            login_user(user)
+            flash('Login successful.', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password.', 'error')
+    return render_template('login.html', form=form)
+
+# Create a route to display posts
 @app.route('/')
 def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
-
+# Create a route to create new posts
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create_post():
     if request.method == 'POST':
         title = request.form['title']
@@ -30,7 +112,6 @@ def create_post():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('create.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
