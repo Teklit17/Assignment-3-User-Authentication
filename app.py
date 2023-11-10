079c55db2ba16d2357f00b59573eeeeee8628ed2
teklit amanuel <teklitag@uia.no>
2023-11-10 04:50:23 +0100

    all the way up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -7,27 +7,46 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import bcrypt
 from flask_wtf.csrf import CSRFProtect
+from authlib.integrations.flask_client import OAuth
 import pyotp
-from PIL import Image
-import io
-from flask import send_file
 import qrcode
-from flask import send_file
-import pyotp
+import secrets
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' # Replace with your Google Client Secret
 app.config['SECRET_KEY'] = 'your-secret-key'  # Replace with a secure secret key
 app.config['WTF_CSRF_ENABLED'] = True  # Enable CSRF protection
 csrf = CSRFProtect(app)
-# Initialize SQLAlchemy and Flask-Migrate
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-# Configure the login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+
+limiter = Limiter(
+    app,
+    default_limits=["200 per day", "50 per hour"]
+)
+
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='154126243374-rotm7jm3v31he80u1is2hcbtum1g07ve.apps.googleusercontent.com',
+    client_secret='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',
+    access_token_url='https://oauth2.googleapis.com/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid email profile'},
+    jwks_uri='https://www.googleapis.com/oauth2/v3/certs'
+)
+
+
 
 # Define RegistrationForm
 class RegistrationForm(FlaskForm):
@@ -35,11 +54,13 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+
 # Define LoginForm
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
 
 # Define a User class
 class User(db.Model, UserMixin):
@@ -47,8 +68,14 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     totp_secret = db.Column(db.String(16))
+    google_id = db.Column(db.String(120), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=True)
     # Relationship to link users to their posts
     posts = db.relationship('Post', backref='author', lazy=True)
+    failed_attempts = db.Column(db.Integer, default=0)
+    lock_until = db.Column(db.DateTime, nullable=True)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
@@ -56,11 +83,23 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', name='fk_user_id'))
 
 
+class VerifyTOTPForm(FlaskForm):
+    totp_token = StringField('TOTP Token', validators=[DataRequired()])
+    submit = SubmitField('Verify')
+
+
+# Define PostForm
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = StringField('Content', validators=[DataRequired()])
+    submit = SubmitField('Create Post')
+
 
 # Create a user loader function
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 # Function to hash passwords
 def hash_password(password):
@@ -68,7 +107,49 @@ def hash_password(password):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')
 
-# Define Post class for the database
+
+# Google OAuth token getter
+
+@app.route('/login/google')
+def login_google():
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/login/google/authorize')
+def authorize():
+    try:
+        token = google.authorize_access_token()
+    except Exception as e:
+        # Handle the error appropriately, maybe log it, and redirect
+        flash("Authorization failed or was cancelled: " + str(e), "error")
+        return redirect(url_for('login'))
+
+    resp = google.get('userinfo', token=token)
+    user_info = resp.json()
+
+    # Check if user exists
+    user = User.query.filter_by(google_id=user_info['id']).first()
+    if not user:
+        # User not found, create a new user
+        # Generate a secure, random placeholder password
+        placeholder_password = secrets.token_hex(16)
+
+        user = User(
+            username=user_info['name'],  # Assuming you have a username field
+            google_id=user_info['id'],
+            email=user_info['email'],
+            password = hash_password(placeholder_password)
+            # You can add other fields here as needed
+        )
+        db.session.add(user)
+        db.session.commit()
+
+    # Log in the user
+    login_user(user, remember=True)
+
+    # Redirect to a desired page, e.g., the home page
+    return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -96,7 +177,8 @@ def register():
         db.session.commit()
 
         # Generate QR code for TOTP
-        totp_uri = pyotp.totp.TOTP(new_user.totp_secret).provisioning_uri(name=new_user.username, issuer_name="YourAppName")
+        totp_uri = pyotp.totp.TOTP(new_user.totp_secret).provisioning_uri(name=new_user.username,
+                                                                          issuer_name="YourAppName")
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -114,41 +196,56 @@ def register():
 
     return render_template('register.html', form=form)
 
+
 @app.route('/display_qr/<username>')
 def display_qr(username):
     path = f'static/qrcodes/{username}.png'
     return send_file(path, mimetype='image/png')
 
+
 @app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("5 per minute")  # Adjust the rate limit as needed
 def login():
     form = LoginForm()
-
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
 
         user = User.query.filter_by(username=username).first()
+        if user:
+            # Check if the account is locked due to failed attempts
+            if user.lock_until and user.lock_until > datetime.utcnow():
+                flash('Account is temporarily locked due to multiple failed login attempts. Please try again later.','error')
+                return render_template('login.html', form=form)
+            # If user registered through Google, disallow password login
+            if user.google_id and not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                flash('Login through Google is required for this account.', 'error')
+                return render_template('login.html', form=form)
 
-        if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-            # If password is correct, redirect to TOTP verification
-            return redirect(url_for('verify_totp', username=user.username))
-        else:
-            flash('Invalid username or password.', 'error')
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                # Reset failed attempts
+                user.failed_attempts = 0
+                user.lock_until = None
+                db.session.commit()
+                # If password is correct, redirect to TOTP verification
+                return redirect(url_for('verify_totp', username=user.username))
+            else:
+                # Increment failed attempts and lock account if necessary
+                user.failed_attempts += 1
+                if user.failed_attempts >= 3:
+                    user.lock_until = datetime.utcnow() + timedelta(minutes=15)  # Lock for 15 minutes
+                db.session.commit()
+                flash('Invalid username or password.', 'error')
 
     return render_template('login.html', form=form)
-
-
-class VerifyTOTPForm(FlaskForm):
-    totp_token = StringField('TOTP Token', validators=[DataRequired()])
-    submit = SubmitField('Verify')
 
 
 @app.route('/verify_totp/<username>', methods=['GET', 'POST'])
 def verify_totp(username):
     user = User.query.filter_by(username=username).first()
     form = VerifyTOTPForm()
-    if request.method == 'POST':
-        totp_token = request.form.get('totp_token')
+    if form.validate_on_submit():
+        totp_token = form.totp_token.data
         totp = pyotp.TOTP(user.totp_secret)
         if totp.verify(totp_token):
             login_user(user)
@@ -159,43 +256,40 @@ def verify_totp(username):
 
     return render_template('verify_totp.html', form=form, username=username)
 
-    # Create a route for user logout
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
+    session.pop('google_token', None)
     return redirect(url_for('login'))
 
-# Create a route to display posts
+
 @app.route('/')
 def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
-# Define PostForm
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = StringField('Content', validators=[DataRequired()])
-    submit = SubmitField('Create Post')
 
-
-# Create a route to create new posts
-# Create a route to create new posts
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create_post():
-    form = PostForm()  # Create an instance of PostForm
+    form = PostForm()
     if form.validate_on_submit():
-
         title = form.title.data
         content = form.content.data
-        # Create a new Post object with the current user's id
         post = Post(title=title, content=content, user_id=current_user.id)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('index'))
+
     return render_template('create.html', form=form)
+
+
+with app.app_context():
+    db.create_all()  # or any other operation that requires app context
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #readme
